05a291b83b9b246d2198815d274992e939893a7c
pratiksha950 <salunkepratiksha950@gmail.com>
2026-03-24 07:17:01 +0530

    inheritance code completed:

---
+++ 9-OOP/Inheritance.java
@@ -0,0 +1,146 @@
+// public class Inheritance {
+//     public static void main(String[] args) {
+//         Fish f=new Fish();
+//         f.eat();
+//     }
+// }
+// //base class
+// class Animal{
+//     void eat(){
+//         System.out.println("animal is eating");
+//     }
+//     void breath(){
+//         System.out.println("breath");
+//     }
+// }
+// //child class
+// class Fish extends Animal { 
+//     void swim(){
+//         System.out.println("swim");
+//     }
+// }
+
+//***Multilevel inheritance***
+// public class Inheritance {
+//     public static void main(String[] args) {
+//         Fish f=new Fish();
+//         f.eat();
+//         Dog d=new Dog();
+//         d.eat();
+//         d.legs=4;
+//         System.out.println( d.legs);
+//     }
+// }
+// //base class
+// class Animal{
+//     void eat(){
+//         System.out.println("animal is eating");
+//     }
+//     void breath(){
+//         System.out.println("breath");
+//     }
+// }
+// //child class
+// class Fish extends Animal { 
+//     void swim(){
+//         System.out.println("swim");
+//     }
+// }
+
+// class Mammels extends Animal { 
+//     int legs;
+// }
+// class Dog extends Mammels{
+//     String breed;
+// }
+
+//***Hierarchical inheritance***
+// public class Inheritance {
+//     public static void main(String[] args) {
+//         Fish f=new Fish();
+//         f.eat();
+//     }
+// }
+// //base class
+// class Animal{
+//     void eat(){
+//         System.out.println("animal is eating");
+//     }
+//     void breath(){
+//         System.out.println("breath");
+//     }
+// }
+// //child class
+// class Fish extends Animal { 
+//     void swim(){
+//         System.out.println("swim");
+//     }
+// }
+
+// class Mammels extends Animal { 
+//     void walk(){
+//         System.out.println("walk");
+//     }
+// }
+// class Bird extends Animal { 
+//     void fly(){
+//         System.out.println("fly");
+//     }
+// }
+
+//***Hybrid Inheritance***/
+// Hybrid Inheritance Example
+
+// Parent 
+interface Animal {
+    void eat();
+}
+
+// Second Parent 
+interface Pet {
+    void play();
+}
+
+// Child Class (Multilevel + Multiple)
+class Dog implements Animal, Pet {
+
+    public void eat() {
+        System.out.println("Dog eats food");
+    }
+
+    public void play() {
+        System.out.println("Dog plays with ball");
+    }
+
+    void bark() {
+        System.out.println("Dog barks");
+    }
+}
+
+// Another Child Class (Hierarchical)
+class Cat implements Animal {
+
+    public void eat() {
+        System.out.println("Cat eats fish");
+    }
+
+    void meow() {
+        System.out.println("Cat meows");
+    }
+}
+
+// Main Class
+public class Inheritance {
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.eat();
+        d.play();
+        d.bark();
+
+        Cat c = new Cat();
+        c.eat();
+        c.meow();
+    }
+}
--- 9-OOP/oop.java
+++ 9-OOP/oop.java
@@ -52,25 +52,113 @@
 // }
 
 //Copy Constructor 
-public class oop {
+// public class oop {
 
-    public static void main(String[] args) {
-        Student s1=new Student();
-        Student s2=new Student("Atharv");
-        Student s3=new Student(23);
-    }
-}
-class Student{
-    String name;
-    int roll;
-    Student(){
-       System.out.println("non parameterized constructor is called");
-    }
-     Student(String name){
-       this.name=name;
-    }
-     Student(int roll){
-       this.roll=roll;
-    }
+//     public static void main(String[] args) {
+//         Student s1=new Student();
+//         s1.name="kartik";
+//         s1.roll=90;
+//         s1.password="abcd";
+//         s1.marks[0]=100;
+//         s1.marks[1]=70;
+//         s1.marks[2]=10;
 
-}
+//         Student s2=new Student(s1);
+//         s2.password="xyz";
+//         s1.marks[2]=30;
+
+//         for(int i=0;i<3;i++){
+//             System.out.println(s2.marks[i]);
+//         }
+        
+//     }
+// }
+// class Student{
+//     String name;
+//     int roll;
+//     String password;
+//     int marks[];
+
+//     //copy constructor
+//     Student(Student s1){
+//         marks=new int[3];
+//         this.name=s1.name;
+//         this.roll=s1.roll;
+//         this.marks=s1.marks;
+//     }
+//     Student(){
+//         marks=new int[3];
+//        System.out.println("non parameterized constructor is called");
+//     }
+//      Student(String name){
+//         marks=new int[3];
+//        this.name=name;
+//     }
+//      Student(int roll){
+//         marks=new int[3];
+//        this.roll=roll;
+//     }
+
+// } 
+
+//Shallow and deep copy
+// public class oop {
+
+//     public static void main(String[] args) {
+//         Student s1=new Student();
+//         s1.name="kartik";
+//         s1.roll=90;
+//         s1.password="abcd";
+//         s1.marks[0]=100;
+//         s1.marks[1]=70;
+//         s1.marks[2]=10;
+
+//         Student s2=new Student(s1);
+//         s2.password="xyz";
+//         s1.marks[2]=30;
+
+//         for(int i=0;i<3;i++){
+//             System.out.println(s2.marks[i]);
+//         }
+        
+//     }
+// }
+// class Student{
+//     String name;
+//     int roll;
+//     String password;
+//     int marks[];
+
+    //shallow copy constructor
+    // Student(Student s1){
+    //     marks=new int[3];
+    //     this.name=s1.name;
+    //     this.roll=s1.roll;
+    //     this.marks=s1.marks;
+    // }
+
+    //deep copy constructor
+//     Student(Student s1){
+//         marks=new int[3];
+//         this.name=s1.name;
+//         this.roll=s1.roll;
+//         for(int i=0;i<marks.length;i++){
+//             this.marks[i]=s1.marks[i];
+//         }
+//     }
+
+//     Student(){
+//         marks=new int[3];
+//        System.out.println("non parameterized constructor is called");
+//     }
+//      Student(String name){
+//         marks=new int[3];
+//        this.name=name;
+//     }
+//      Student(int roll){
+//         marks=new int[3];
+//        this.roll=roll;
+//     }
+
+// } 
+
