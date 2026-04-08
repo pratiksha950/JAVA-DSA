84a8990b3d52d26f8d1ca73a310c09b46be877cc
pratiksha950 <salunkepratiksha950@gmail.com>
2026-04-08 20:36:06 +0530

    Queen program added:

--- 13-Backtracking/all.java
+++ 13-Backtracking/all.java
@@ -28,7 +28,7 @@ public class all {
 }
 */
 
-/*Backtracking FOR finding subset */
+/*Backtracking FOR finding subset 
 public class all {
     public static void findSubArr(String str,String ans,int i) {
         //base case
@@ -54,4 +54,29 @@ public class all {
     String str="abc";
     findSubArr(str,"",0);
 }
+} */
+
+/*Find Permutation code TC= O(n * n!)
+public class all { 
+    public static void findPermutation(String str,String ans) {
+        //base case
+       if(str.length()==0){
+        System.out.println(ans);
+        return;
+       }
+
+        //recursion
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+            //"abcde"="ab"+"de"
+            String NewStr=str.substring(0,i)+str.substring(i+1);
+             findPermutation(NewStr,ans+curr);
+        }
+
+    }
+
+    public static void main(String[] args) {
+    String str="abc";
+    findPermutation(str,"");
 }
+}*/
